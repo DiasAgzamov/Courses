0c143b7a7538f7adf0f53497a687e482a1b52073
DiasAgzamov <66073388+DiasAgzamov@users.noreply.github.com>
2020-08-06 14:34:56 +0600

    Add files via upload

---
+++ second_entry/sec_entry.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <string>
+#include <algorithm>
+
+int main() {
+    std::string s;
+    std::cin >> s;
+    int a = count(begin(s), end(s), 'f');
+    int x = 0;
+    if (a == 0) {
+        std::cout << "-2" << std::endl;
+    } else if (a == 1) {
+        std::cout << "-1" << std::endl;
+    } else {
+        for (int i = 0; i < s.size(); i++) {
+            if (s[i] == 'f') {
+                x++;
+            }
+            if (x == 2) {
+                std::cout << i << std::endl;
+                break;
+            }
+        }
+    }
+
+    return 0;
+}
